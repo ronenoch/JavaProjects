97119aee87e07a619aaa6fe8ed0e01b63ee24663
Ron Enoch <ronenoch13@gmail.com>
2023-03-21 18:28:22 +0200

    Another renaming
    Signed-off-by: Ron Enoch <ronenoch13@gmail.com>

--- a/src/Main.java
+++ a/src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        CashRegister cashRegister = new CashRegister(300.0);
+        CashDesk cashDesk = new CashDesk(300.0);
         Scanner sc = new Scanner(System.in);
 
         boolean exit = false;
@@ -26,27 +26,27 @@ public class Main {
                     double price = sc.nextDouble();
                     System.out.print("Please enter the item's quantity: ");
                     int quantity = sc.nextInt();
-                    cashRegister.addNewItem(new Item(name, price), quantity);
+                    cashDesk.addNewItem(new Item(name, price), quantity);
                     break;
 
                 case 2:
                     System.out.print("Please enter the amount received from the customer: ");
                     double customerAmount = sc.nextDouble();
-                    if (cashRegister.getCurrentPurchaseAmount() > customerAmount) {
+                    if (cashDesk.getCurrentPurchaseAmount() > customerAmount) {
                         System.out.println("You can't pay less then the price!");
                         break;
                     }
-                    double change = cashRegister.receivePayment(customerAmount);
+                    double change = cashDesk.receiveBillPayment(customerAmount);
                     System.out.println("The customer's change is: " + change);
                     break;
 
                 case 3:
                     System.out.println("The current purchase details:");
-                    System.out.println(cashRegister);
+                    System.out.println(cashDesk);
                     break;
 
                 case 4:
-                    System.out.println("The total amount of money in the register is: " + cashRegister.getTotalCash());
+                    System.out.println("The total amount of money in the register is: " + cashDesk.getTotalCash());
                     break;
 
                 case 5:
