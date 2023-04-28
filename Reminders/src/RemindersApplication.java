fb3bdc9644d84a7c0fe1eec39e4e90b832a34238
Ron Enoch <ronenoch13@gmail.com>
2023-04-28 22:17:35 +0300

    organized the code.
    Signed-off-by: Ron Enoch <ronenoch13@gmail.com>

--- Reminders/src/RemindersApplication.java
+++ Reminders/src/RemindersApplication.java
@@ -5,12 +5,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MenuApplication extends Application {
+public class RemindersApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("menu-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(RemindersApplication.class.getResource("reminders-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 500);
-        stage.setTitle("Menu");
+        stage.setTitle("Reminders");
         stage.setScene(scene);
         stage.show();
     }
--- Reminders/src/RemindersController.java
+++ Reminders/src/RemindersController.java
@@ -1,19 +1,15 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
-//import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import javax.swing.*;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MenuController {
+public class RemindersController {
     @FXML
     private TextField textF;
 
@@ -33,106 +29,70 @@ public class MenuController {
 
     private String remindersFileName;
 
-    public void initialize() throws IOException {
-//        this.order = new Order();
-//        this.checkBoxes = new ArrayList<CheckBox>();
+    public void initialize() {
+
         this.reminders = new HashMap<>();
 
         this.remindersFileName = JOptionPane.showInputDialog("Enter Reminders file name");
         loadDataFromFile(remindersFileName);
 
-
-//        InitializeOrder();
-
-        dayBox = new ComboBox<Integer>();
         dayBox.setPromptText("day");
-        dayBox.setOnAction(new EventHandler<ActionEvent>() {
+        /*dayBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                /* Giving the item to the handler to make it be able to update the order */
                 dayBoxPressed(e);
             }
-        });
+        });*/
 
-        /* adding options for the amount choice. */
+        /* adding options for the day choice. */
         for (int i = 1; i < 32; i++) {
             dayBox.getItems().add(i);
         }
-        v_box.getChildren().add(dayBox);
 
-        monthBox = new ComboBox<Integer>();
         monthBox.setPromptText("month");
-        monthBox.setOnAction(new EventHandler<ActionEvent>() {
+        /* monthBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                /* Giving the item to the handler to make it be able to update the order */
                 monthBoxPressed(e);
             }
-        });
+        }); */
 
-        /* adding options for the amount choice. */
+        /* adding options for the month choice. */
         for (int i = 1; i < 13; i++) {
             monthBox.getItems().add(i);
         }
-        v_box.getChildren().add(monthBox);
 
-
-        yearBox = new ComboBox<Integer>();
         yearBox.setPromptText("year");
-        yearBox.setOnAction(new EventHandler<ActionEvent>() {
+        /*yearBox.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                /* Giving the item to the handler to make it be able to update the order */
+                *//* Giving the item to the handler to make it be able to update the order *//*
                 yearBoxPressed(e);
             }
-        });
+        });*/
 
-        /* adding options for the amount choice. */
+        /* adding options for the year choice. */
         for (int i = 2022; i < 2040; i++) {
             yearBox.getItems().add(i);
         }
-        v_box.getChildren().add(yearBox);
 
     }
 
-    private void monthBoxPressed(ActionEvent event) {
-        ComboBox comboBox = (ComboBox) event.getSource();
-        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
-    }
-
-    private void yearBoxPressed(ActionEvent event) {
-        ComboBox comboBox = (ComboBox) event.getSource();
-        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
-        
-    }
-
-    private void dayBoxPressed(ActionEvent event) {
-        ComboBox comboBox = (ComboBox) event.getSource();
-        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
-    }
-
-    private void InitializeOrder() {
-//        for (Map.Entry<String, ArrayList<Item>> entry : this.order.getMenu().getItems().entrySet()) {
-//            Label type = new Label(entry.getKey()) ;
-//            v_box.getChildren().add(type);
-//            for (Item item : entry.getValue()) {
-//                HBox h_box = new HBox(2);
-//                h_box.setSpacing(30);
-//                CheckBox checkBox = new CheckBox(item.getDescription() + "   price: " + String.valueOf(item.getPrice()));
-//                checkBox.setOnAction(new EventHandler<ActionEvent>() {
-//                    @Override public void handle(ActionEvent e) {
-//                        checkBoxPressed(e, item);
-//                    }
-//                });
+//    private void monthBoxPressed(ActionEvent event) {
+//        ComboBox comboBox = (ComboBox) event.getSource();
+//        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
+//    }
 //
-//                this.checkBoxes.add(checkBox);
-//                h_box.getChildren().add(checkBox);
+//    private void yearBoxPressed(ActionEvent event) {
+//        ComboBox comboBox = (ComboBox) event.getSource();
+//        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
 //
-//                v_box.getChildren().add(h_box);
-//            }
-//        }
-    }
+//    }
+//
+//    private void dayBoxPressed(ActionEvent event) {
+//        ComboBox comboBox = (ComboBox) event.getSource();
+//        int selectedOption = (int) comboBox.getSelectionModel().getSelectedItem();
+//    }
 
     protected void writeDataToFile(String fileName){
         try {
-//            FileWriter myWriter = new FileWriter(fileName);
             FileOutputStream myWriter = new FileOutputStream(fileName);
             ObjectOutputStream out = new ObjectOutputStream(myWriter);
             out.writeObject(this.reminders);
@@ -140,25 +100,23 @@ public class MenuController {
             out.close();
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "File IO error", "Error", JOptionPane.ERROR_MESSAGE);
-//            throw e;
         }
     }
 
-    protected void loadDataFromFile(String fileName){
+    protected void loadDataFromFile(String fileName) {
         try {
             FileInputStream myReader = new FileInputStream(fileName);
             ObjectInputStream input = new ObjectInputStream(myReader);
             this.reminders = (HashMap<Date, String>) input.readObject();
             myReader.close();
             input.close();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "File IO error", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (IOException ignored) {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
     @FXML
-    protected void saveReminder() throws IOException {
+    protected void saveReminder() {
         int day = 0;
         int month = 0;
         int year  = 0;
@@ -187,7 +145,7 @@ public class MenuController {
         int day = 0;
         int month = 0;
         int year  = 0;
-        String reminder = new String();
+        String reminder;
 
         try {
             day = this.dayBox.getSelectionModel().getSelectedItem();
